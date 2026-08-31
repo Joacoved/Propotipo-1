6a1273fd6923d5385cb67700f87650ed434bc283
Joacoved <167360161+Joacoved@users.noreply.github.com>
2026-08-31 11:53:33 -0300

    Crear estructura inicial
    -Se crea main.lua

---
+++ main.lua
@@ -0,0 +1,22 @@
+function love.load()
+
+    love.window.setMode(
+        800,
+        600
+    )
+
+    love.window.setTitle(
+        "Prototipo1"
+    )
+
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+
+end
